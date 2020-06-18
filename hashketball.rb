e9d309fca66216c2b458e364cd3a7d5b5cab4530
Mariamol James <mariamol.james@gmail.com>
2020-06-18 20:10:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -223,6 +223,8 @@ def big_shoe_rebounds
         big=x[:shoe]
         answer=x[:rebounds]
       end
+    end
+  end
     game_hash[game_hash.keys[count]][:players].each do |key,value|
       if value.eql?(player)
         answer=game_hash[game_hash.keys[count]][:players][:points]
